a5d38fc564e16c10c7a9f16e1ca4d9012688acbd
Kumar Ishan <kumar.ishan89@gmail.com>
2015-10-16 17:24:09 +0530

    update: added leaves to item type group

--- catalogue/src/main/scala/commons/catalogue/ItemTypeGroup.scala
+++ catalogue/src/main/scala/commons/catalogue/ItemTypeGroup.scala
@@ -11,6 +11,8 @@ trait ItemTypeGroup {
   def name: String
   val id = stringHash(name, symmetricSeed)
   def groups: Seq[ItemTypeGroup]
+  def isLeaf: Boolean
+  def leaf: this.type
 
   /** See if this type is an ancestor for the given type
     * (basically looks into the groups hierarchy)
@@ -28,29 +30,45 @@ object ItemTypeGroup {
 
   private val vmap = scala.collection.mutable.Map[Int, ItemTypeGroup]()
   private val nmap = scala.collection.mutable.Map[String, ItemTypeGroup]()
+  private val _leaves = scala.collection.mutable.Set.empty[ItemTypeGroup]
 
   def apply(id: Int) = vmap(id)
   def apply(name: String) = nmap(name)
 
+  def leaves: Seq[ItemTypeGroup] = _leaves.toSeq
+
   case class ItemTypeGroupVal(name: String, parent: ItemTypeGroup = null) extends ItemTypeGroup {
 
-    override def toString: String = name
-
+    assert(parent == null || !parent.isLeaf)
     assert(!vmap.isDefinedAt(id), "Duplicate id: " + id)
+
     vmap(id) = this
     nmap(toString) = this
 
     val groups: Seq[ItemTypeGroup] = if(parent != null) parent.groups :+ this else IndexedSeq(this)
+    private var _isLeaf = false
+
+    def leaf = {
+      _leaves += this
+      _isLeaf = true
+      this
+    }
+
+    def isLeaf = _isLeaf
+
+    override def toString: String = name
+
   }
 
   implicit class CreateInstance(name: String) {
     def ~>(g: ItemTypeGroup): ItemTypeGroup = ItemTypeGroupVal(name, g)
   }
 
+
   val Clothing       = "Clothing" ~> null
   val MensClothing   = "MensClothing" ~> Clothing
-  val MensTShirt     = "MensTShirt" ~> MensClothing
+  val MensTShirt     = "MensTShirt" ~> MensClothing leaf
 
   val WomensClothing = "WomensClothing" ~> Clothing
-  val WomensTops     = "WomensTops" ~> WomensClothing
+  val WomensTops     = "WomensTops" ~> WomensClothing leaf
 }
