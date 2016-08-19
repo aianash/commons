a3eab5d5ce57561b1b3187735886d69dff6ca3ac
Neeraj Gangwar <y.neeraj2008@gmail.com>
2016-08-19 13:07:44 +0530

    rename: case classes

--- events/src/main/scala/aianonymous/commons/events/TrackingEvents.scala
+++ events/src/main/scala/aianonymous/commons/events/TrackingEvents.scala
@@ -2,26 +2,28 @@ package aianonymous.commons.events
 
 case class Position(x: Int, y: Int)
 
-case class ViewEvent(
+sealed trait TrackingEvent
+
+case class PageFragmentView(
   scrollPos    : Position,
   mousePos     : Position,
   startTime    : Long,
   duration     : Int,
   windowHeight : Int,
   windowWidth  : Int
-  )
+  ) extends TrackingEvent
 
-case class SectionViewEvent(
+case class SectionView(
   sectionId : Int,
   pos       : Position,
   startTime : Long,
   duration  : Int
-  )
+  ) extends TrackingEvent
 
-case class PathEvent(
+case class MousePath(
   mouseStart : Position,
   mouseEnd   : Position,
   sectionIds : Seq[Int],
   startTime  : Long,
   duration   : Int
-  )
+  ) extends TrackingEvent
