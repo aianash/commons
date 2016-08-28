c0162f00b31e3f22542d149be74003ca0a2bf30a
Neeraj Gangwar <y.neeraj2008@gmail.com>
2016-08-28 12:03:14 +0530

    rename: Settings.scala -> PageTags.scala

--- customer/src/main/scala/aianonymous/commons/customer/PageTags.scala
+++ customer/src/main/scala/aianonymous/commons/customer/PageTags.scala
@@ -1,10 +1,8 @@
 package aianonymous.commons.customer
 
-sealed trait Settings
-
 case class PageTags(
   tokenId   : Long,
   pageId    : Long,
   sectionId : Int,
   tags      : Set[String]
-  ) extends Settings
+  )
