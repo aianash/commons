3a01888882a18555eb4e3bcf601b52b70d8542d9
Neeraj Gangwar <y.neeraj2008@gmail.com>
2016-08-29 11:08:06 +0530

    add: json combinators for PageTags

--- customer/src/main/scala/aianonymous/commons/customer/PageTags.scala
+++ customer/src/main/scala/aianonymous/commons/customer/PageTags.scala
@@ -1,8 +1,27 @@
 package aianonymous.commons.customer
 
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+
 case class PageTags(
   tokenId   : Long,
   pageId    : Long,
   sectionId : Int,
   tags      : Set[String]
   )
+
+
+trait PageTagsJsonCombinator {
+
+  protected implicit val pageTagsFormat: Format[PageTags] = (
+    (__ \ "tid").format[Long] and
+    (__ \ "pid").format[Long] and
+    (__ \ "sid").format[Int] and
+    (__ \ "tags").format[Set[String]]
+  ) ((tid, pid, sid, tags) =>
+      PageTags(tid, pid, sid, tags),
+      (a: PageTags) => (a.tokenId, a.pageId, a.sectionId, a.tags)
+  )
+
+}
